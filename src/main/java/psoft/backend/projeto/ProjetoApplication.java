34edc38be53ddfa7ff2c19a74ac1738e3ffcfdf9
Arthur Guedes <arthur.guedes@ccc.ufcg.edu.br>
2019-11-02 16:13:31 -0300

    Substitui código com anotação Bean

--- src/main/java/psoft/backend/projeto/ProjetoApplication.java
+++ src/main/java/psoft/backend/projeto/ProjetoApplication.java
@@ -4,17 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import psoft.backend.projeto.filtros.TokenFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class ProjetoApplication {
 
 	@Bean
-	public FilterRegistrationBean<TokenFilter> filterJwt() {
-		FilterRegistrationBean<TokenFilter> filterRB = new FilterRegistrationBean<>();
-		filterRB.setFilter(new TokenFilter());
-		filterRB.addUrlPatterns("/v1/usuarios");
-		return filterRB;
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
 	}
 
 	public static void main(String[] args) {
