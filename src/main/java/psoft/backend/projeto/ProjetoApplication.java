5a6ef00ec46c4935a42dbf4d1f8c4cbea4584057
Arthur Guedes <arthur.guedes@ccc.ufcg.edu.br>
2019-11-02 16:03:53 -0300

    Adiciona notação Bean na aplicação com filtro

--- src/main/java/psoft/backend/projeto/ProjetoApplication.java
+++ src/main/java/psoft/backend/projeto/ProjetoApplication.java
@@ -2,9 +2,20 @@ package psoft.backend.projeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import psoft.backend.projeto.filtros.TokenFilter;
 
 @SpringBootApplication
 public class ProjetoApplication {
+
+	@Bean
+	public FilterRegistrationBean<TokenFilter> filterJwt() {
+		FilterRegistrationBean<TokenFilter> filterRB = new FilterRegistrationBean<>();
+		filterRB.setFilter(new TokenFilter());
+		filterRB.addUrlPatterns("/v1/usuarios");
+		return filterRB;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoApplication.class, args);
