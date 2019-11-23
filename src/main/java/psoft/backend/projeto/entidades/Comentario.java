a474a3d013a3e065bbf08f83a2e5e84c25f2fbb4
danilomfreitas <danilo.menezes.freitas@ccc.ufcg.edu.br>
2019-11-23 10:51:30 -0300

    Refatorando entidade Comentario

--- src/main/java/psoft/backend/projeto/entidades/Comentario.java
+++ src/main/java/psoft/backend/projeto/entidades/Comentario.java
@@ -1,28 +1,41 @@
 package psoft.backend.projeto.entidades;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class Comentario {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private long idComentario;
 
     private String msg;
+    private String emailUsuarioDono;
+    private boolean apagado;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "idCampanha")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Campanha campanha;
+
+    @OneToOne
+    private Comentario comentarioPai;
+
+    @OneToMany(mappedBy = "comentarioPai")
+    private List<Comentario> respostas;
 
     public Comentario() {
     }
 
-    public long getId() {
-        return id;
+    public long getIdComentario() {
+        return idComentario;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setIdComentario(long id) {
+        this.idComentario = id;
     }
 
     public String getMsg() {
@@ -39,5 +52,50 @@ public class Comentario {
 
     public void setCampanha(Campanha campanha) {
         this.campanha = campanha;
+    }
+
+    public Comentario getComentarioPai() {
+        return comentarioPai;
+    }
+
+    public void setComentarioPai(Comentario comentarioPai) {
+        this.comentarioPai = comentarioPai;
+    }
+
+    public List<Comentario> getRespostas() {
+        return respostas;
+    }
+
+    public void setRespostas(List<Comentario> respostas) {
+        this.respostas = respostas;
+    }
+
+    public String getEmailUsuarioDono() {
+        return emailUsuarioDono;
+    }
+
+    public boolean isApagado() {
+        return apagado;
+    }
+
+    public void setApagado(boolean apagado) {
+        this.apagado = apagado;
+    }
+
+    public void setEmailUsuarioDono(String emailUsuarioDono) {
+        this.emailUsuarioDono = emailUsuarioDono;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comentario that = (Comentario) o;
+        return idComentario == that.idComentario;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idComentario);
     }
 }
